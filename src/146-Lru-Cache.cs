30e481ca2ec9b644488fcd7cac0e02de652e4877
esc1896 <esc1896@163.com>
2019-03-15 22:07:49 +0800

    LRU-146-HaspMap-DoubleLinkedList

--- src/146-Lru-Cache.cs
+++ src/146-Lru-Cache.cs
@@ -1,3 +1,124 @@
+// Second version: HashMap + Double Linked List
+// Runtime: 256 ms
+// Memory Usage: 48 MB
+
+public class LRUCache
+{
+    public class node
+    {
+        public int mKey;
+        public int mValue;
+        public node mPrev;
+        public node mNext;
+
+        public node(int key, int value)
+        {
+            mKey = key;
+            mValue = value;
+            mPrev = null;
+            mNext = null;
+        }
+    }
+
+    private int count;
+    private int cap;
+    private node head, tail;
+    private Dictionary<int, node> dict;
+
+    public LRUCache(int capacity)
+    {
+        count = 0;
+        cap = capacity;
+        head = new node(0, 0);
+        tail = new node(0, 0);
+        dict = new Dictionary<int, node>();
+    }
+
+    private void insertNodeAfterHead(node tmp)
+    {
+        tmp.mNext = head.mNext;
+        head.mNext.mPrev = tmp;
+
+        tmp.mPrev = head;
+        head.mNext = tmp;
+    }
+
+    private void moveNodeAfterHead(node tmp)
+    {
+        tmp.mPrev.mNext = tmp.mNext;
+        tmp.mNext.mPrev = tmp.mPrev;
+
+        insertNodeAfterHead(tmp);
+    }
+
+    public int Get(int key)
+    {
+        int rst = -1;
+        if (dict.Keys.Contains(key))
+        {
+            node tmp = dict[key];
+
+            moveNodeAfterHead(tmp);
+
+            rst = dict[key].mValue;
+        }
+        return rst;
+    }
+
+    public void Put(int key, int value)
+    {
+        if (cap == 0) return;
+
+        // node in list
+        if (dict.Keys.Contains(key))
+        {
+            node tmp = dict[key];
+            tmp.mValue = value;
+
+            moveNodeAfterHead(tmp);
+        }
+        else
+        {
+            // count < cap, add new node at head
+            if (count < cap)
+            {
+                node tmp = new node(key, value);
+
+                // First node, link to tail as well
+                if (count == 0)
+                {
+                    head.mNext = tmp;
+                    tmp.mPrev = head;
+
+                    tmp.mNext = tail;
+                    tail.mPrev = tmp;
+                }
+                else
+                {
+                    insertNodeAfterHead(tmp);
+                }
+
+                dict.Add(key, tmp);
+                count++;
+            }
+            // count == cap, remove tail, add new node at head                        
+            else
+            {
+                dict.Remove(tail.mPrev.mKey);
+                node prevPrev = tail.mPrev.mPrev;
+                prevPrev.mNext = tail;
+                tail.mPrev = prevPrev;
+
+                node tmp = new node(key, value);
+                insertNodeAfterHead(tmp);
+                dict.Add(key, tmp);
+            }
+        }
+        return;
+    }
+}
+
+
 // First version: Single linked list
 // Runtime: 476 ms
 // Memory Usage: 47.6 MB
